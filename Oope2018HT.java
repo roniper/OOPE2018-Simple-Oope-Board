90460aded3be8cd4d7f7bffd8330b431c21c781c
Roni Perälä <36543508+roniper@users.noreply.github.com>
2019-01-13 04:06:58 +0200

    Add files via upload

---
+++ Oope2018HT.java
@@ -0,0 +1,15 @@
+import oope2018ht.kaytto.*;
+
+/**
+ *
+ * @author Roni
+ */
+public class Oope2018HT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TUI kaynnista = new TUI();
+    }
+}
